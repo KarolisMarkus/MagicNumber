219dc2fc3c5a40007fb0257390c87bd0579ce988
Karolis Markuskis <karolis.markuskis@gmail.com>
2019-10-02 13:07:51 +0300

    still unfinished, need to check if all multiplications have conditions of magical number

--- mavenproject1/src/main/java/lt/bit/magicnumber/main/main.java
+++ mavenproject1/src/main/java/lt/bit/magicnumber/main/main.java
@@ -12,8 +12,8 @@ import java.util.Objects;
 import java.util.Scanner;
 
 /**
- * unfinished program to get magic number from input it checks if multiplied from 1 to 6
- * number still have magic number properties
+ * unfinished program to get magic number from input it checks if multiplied
+ * from 1 to 6 number still have magic number properties
  *
  * @author Karolis
  */
@@ -22,7 +22,9 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Enter a number");
         Scanner sc = new Scanner(System.in);
+        //smn - magic number string
         String smn = sc.nextLine();
+        //mn - magic number
         Integer mn = null;
         try {
             mn = Integer.parseInt(smn);
@@ -31,25 +33,38 @@ public class main {
             System.out.println("blogai ivestas skaicius");
         }
         if (mn != null) {
-            System.out.println("numeris yra " + mn);
             List<Integer> mnl = toIntegerArray(mn);
-            System.out.println(mnl);
             if (mnl != null) {
+                //isMagical - checks if number is magical
                 Boolean isMagical = false;
+                //magic number digit count is the same as times number have to be multiplied
+                //this loop checks all multiplications
                 for (int i = 1; i <= mnl.size(); i++) {
-                    List<Integer> tempList = new ArrayList<>();
                     List<Integer> multipliedList = toIntegerArray(mn * i);
+                    //checking if multiplied list have first original number digit
                     List<Integer> indexes = getIndexes(multipliedList, mnl.get(0));
-                    if (!indexes.isEmpty()){
-                        indexes.forEach((index) -> {
-                            for(int j = index; j<multipliedList.size(); j++){
-                                
+                    if (!indexes.isEmpty()) {
+                        //array list of indexes, for all repeated 
+                        for (Integer index : indexes) {
+                            // two cycles to get from index to last and from first to index elements of array
+                            List<Integer> tempList = new ArrayList<>();
+                            for (int j = index; j < multipliedList.size(); j++) {
+                                tempList.add(multipliedList.get(j));
                             }
-                        }); 
-                        //take a original number array and check if it's have the same order in multiplied number array from index i to last digit
-                        //and from first digit to index
-                    }else break;
+                            for (int j = 0; j < index; j++) {
+                                tempList.add(multipliedList.get(j));
+                            }
+                            if (mnl.equals(tempList)) {
+                                isMagical = true;
+                                break;
+                            }
+                        }
+
+                    } else {
+                        break;
+                    }
                 }
+
             }
         }
     }
@@ -73,13 +88,13 @@ public class main {
         Collections.reverse(numbers);
         return numbers;
     }
-    
-    public static List<Integer> getIndexes(List<Integer> multiList, Integer number){
+
+    public static List<Integer> getIndexes(List<Integer> multiList, Integer number) {
         List<Integer> l = new ArrayList();
-        for (int i= 0; i<multiList.size(); i++) {
-            if(Objects.equals(multiList.get(i), number)){
+        for (int i = 0; i < multiList.size(); i++) {
+            if (Objects.equals(multiList.get(i), number)) {
                 l.add(i);
-            }            
+            }
         }
         return l;
     }
